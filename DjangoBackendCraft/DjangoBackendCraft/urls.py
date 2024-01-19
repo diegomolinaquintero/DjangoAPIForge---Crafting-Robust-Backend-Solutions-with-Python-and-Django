5b2a7ac70bb677d61cff141d3df8a5495d008a20
diegomolinaquintero <molinaquintero14@gmail.com>
2024-01-19 16:04:41 -0500

    Add index

--- DjangoBackendCraft/DjangoBackendCraft/urls.py
+++ DjangoBackendCraft/DjangoBackendCraft/urls.py
@@ -19,6 +19,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 
 urlpatterns = [
+    path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('search/', include('search.urls')),
